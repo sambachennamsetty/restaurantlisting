47fca7596f7cddd6c03d07a54afb018a198b0c97
sambachennamsetty <nvshsamba@gmail.com>
2024-09-08 01:20:34 -0400

    sysout added

--- src/test/java/com/codedecode/restaurantlisting/RestaurantlistingApplicationTests.java
+++ src/test/java/com/codedecode/restaurantlisting/RestaurantlistingApplicationTests.java
@@ -1,15 +1,9 @@
 package com.codedecode.restaurantlisting;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class RestaurantlistingApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // This test will pass if the application context is loaded successfully
-    }
 }
