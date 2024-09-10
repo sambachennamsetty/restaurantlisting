758f96f0bc4e3d8982df9281a90a513dc235e4a8
sambachennamsetty <nvshsamba@gmail.com>
2024-09-09 22:16:43 -0400

    delete main class test file

--- src/test/java/com/codedecode/restaurantlisting/RestaurantlistingApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.codedecode.restaurantlisting;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RestaurantlistingApplicationTests {
-
-}
