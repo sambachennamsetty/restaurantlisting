03f9203d91713cf043912f51e3f6286348c572e2
sambachennamsetty <nvshsamba@gmail.com>
2024-09-09 22:55:40 -0400

    test cases

--- src/main/java/com/codedecode/restaurantlisting/dto/RestaurantDTO.java
+++ src/main/java/com/codedecode/restaurantlisting/dto/RestaurantDTO.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/codedecode/restaurantlisting/entity/Restaurant.java
+++ src/main/java/com/codedecode/restaurantlisting/entity/Restaurant.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@EqualsAndHashCode
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
